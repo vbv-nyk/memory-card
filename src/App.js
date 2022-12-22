9606f267ebce877d476033e820d27a204c6d37a1
Vaibhav Nayak <vbv.nyk.2003@gmail.com>
2022-12-22 19:26:10 +0530

    Add score and highscore

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
+import { useEffect, useState } from 'react';
 import './App.css';
-
-
+import { cardData } from './CardsData';
+function CardsContainer({ children }) {
+  return (
+    <div className='card-container'>
+      {children}
+    </div>
+  );
+}
+function Card({ data }) {
+  const [clicked, setClicked] = useState(false);
+  useEffect(() => {
+    const cardContainer = document.querySelector(".card-container");
+    for (let i = cardContainer.children.length; i >= 0; i--) {
+      cardContainer.appendChild(cardContainer.children[Math.random() * i | 0]);
+    }
+  }, [clicked]);
+  function updateScore() {
+    if (clicked) {
+      data.setHighScore(Math.max(data.score, data.highScore));
+      data.setScore(0);
+      setClicked(false);
+    } else {
+      data.setScore(n => n + 1);
+      setClicked(true);
+    }
+  }
+  return (<div onClick={updateScore} className="card">
+    <div className="image-name">{data.name}</div>
+    <img src={data.source} alt="" />
+  </div>)
+}
+function ScoreBoard({ data }) {
+  return (<>
+    <div className='score-text'>{`Your current score is: ${data.score}`}</div>
+    <div className='high-score-text'>{`Your Highscore is: ${data.highScore}`}</div>
+  </>)
+}
 
 function App() {
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const props = { score, highScore, setScore, setHighScore };
+  function loadCards() {
+    const Cards = cardData.map(card => {
+      return <Card data={{ ...card, ...props }} key={card.key} />
+    })
+    return Cards;
+  }
   return (
     <div>
-
+      <ScoreBoard data={{ ...props }} />
+      <CardsContainer>
+        {loadCards()}
+      </CardsContainer>
     </div>
   );
 }
---
+++ src/CardsData.js
@@ -0,0 +1,52 @@
+export const cardData = [
+    {
+        key: 1,
+        name: 'Couple Sitting Together',
+        source: 'https://images.pexels.com/photos/14683126/pexels-photo-14683126.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 2,
+        name: 'Heart On Snow',
+        source: 'https://images.pexels.com/photos/6372804/pexels-photo-6372804.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 3,
+        name: 'Girl In Library',
+        source: 'https://images.pexels.com/photos/14794875/pexels-photo-14794875.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 4,
+        name: 'Antelope In Snow',
+        source: 'https://images.pexels.com/photos/11220223/pexels-photo-11220223.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 5,
+        name: 'Vehicles On Road',
+        source: 'https://images.pexels.com/photos/14715138/pexels-photo-14715138.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 6,
+        name: 'Sitting By The Bridge',
+        source: 'https://images.pexels.com/photos/14116777/pexels-photo-14116777.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 7,
+        name: 'House By The Sea',
+        source: 'https://images.pexels.com/photos/14768797/pexels-photo-14768797.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 8,
+        name: 'Tree With No Leaves',
+        source: 'https://images.pexels.com/photos/2824673/pexels-photo-2824673.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 9,
+        name: 'Monument',
+        source: 'https://images.pexels.com/photos/13999202/pexels-photo-13999202.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
+    },
+    {
+        key: 10,
+        name: 'Cat By The Tree',
+        source: 'https://images.pexels.com/photos/14798149/pexels-photo-14798149.jpeg?auto',
+    },
+]  
